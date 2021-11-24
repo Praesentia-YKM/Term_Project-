effc8f07e4cd3e6b961a275f6a3cd5b274315073
Praesentia-YKM <68469694+Praesentia-YKM@users.noreply.github.com>
2021-11-24 18:24:25 +0900

    TextDetecting.java

---
+++ Text_detecting/src/GoogleVisionApiTester.java
@@ -0,0 +1,42 @@
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+sdfsd
+public class GoogleVisionApiTester {
+	public static void main(String[] args) {
+		try {
+			String imageFilePath = "C:\\Users\\ykm88\\OneDrive\\바탕 화면\\TermProject\\plastic.png"; // 여기에는 자신의 로컬 이미지 명이 들어가야합니다.
+			List<AnnotateImageRequest> requests = new ArrayList<>();
+			ByteString imgBytes = ByteString.readFrom(new FileInputStream(imageFilePath));
+			Image img = Image.newBuilder().setContent(imgBytes).build();
+			Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+			AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+			requests.add(request);
+			try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+				BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+				List<AnnotateImageResponse> responses = response.getResponsesList();
+				for (AnnotateImageResponse res : responses) {
+					if (res.hasError()) {
+						System.out.printf("Error: %s\n", res.getError().getMessage());
+						return;
+					}
+
+					System.out.println("Text : ");
+					System.out.println(res.getTextAnnotationsList().get(0).getDescription());
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
